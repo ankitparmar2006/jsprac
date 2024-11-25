7446338f7466b798e99611cde0e54bc00894e4dc
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-11-25 23:46:20 +0530

    new work

---
+++ array.js
@@ -0,0 +1,14 @@
+function arr2 (){
+
+let fruit=["Appple","mango","banana","graps","papaya"]
+
+//fruit.pop()
+//fruit.push ("papita")
+//fruit.shift ();
+//fruit.unshift ("ayush");
+//fruit.splice(1,2,"sonu")
+console.log(fruit.slice(2,4))
+
+console.log (fruit)
+}
+arr2 ();
