7bdca921bd41953ede9879f64673f5c0add44f89
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-11-26 23:50:03 +0530

    update

--- array.js
+++ array.js
@@ -1,14 +1,14 @@
-function arr2 (){
+// function arr2 (){
 
-let fruit=["Appple","mango","banana","graps","papaya"]
+// let fruit=["Appple","mango","banana","graps","papaya"]
 
-//fruit.pop()
-//fruit.push ("papita")
-//fruit.shift ();
-//fruit.unshift ("ayush");
-//fruit.splice(1,2,"sonu")
-console.log(fruit.slice(2,4))
+// //fruit.pop()
+// //fruit.push ("papita")
+// //fruit.shift ();
+// //fruit.unshift ("ayush");
+// //fruit.splice(1,2,"sonu")
+// console.log(fruit.slice(2,4))
 
-console.log (fruit)
-}
-arr2 ();
+// console.log (fruit)
+// }
+// arr2 ();
---
+++ string.js
@@ -0,0 +1,10 @@
+ run =()=>{
+
+let fruit =["Apple","mango" ,"bananaa", "Orange","graps","chukunder"]   
+
+fruit.splice(2,1);
+
+console.log(fruit)
+
+}
+run ();
