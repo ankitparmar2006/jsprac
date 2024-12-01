813573a246ea06b5d6ba36eb824a0bcce3cb34fb
ankitparmar2006 <ankitparmar30202@gmail.com>
2024-12-01 23:35:51 +0530

    ques

---
+++ dec.js
@@ -0,0 +1,102 @@
+let run1 =()=>{
+
+let age=prompt("enter your age");
+
+    let text1=document.querySelector=("#text1");
+
+let result=age>=18 ? "grater":"smaller";
+
+alert (result);
+
+    
+
+
+}
+
+// -------------------------------------------------------------------
+
+let run2 =()=>{
+
+    let ans1=confirm("course completed ?");
+
+    let ans2=confirm("certificate completed ?");
+
+if (ans1 && ans2 ){
+    
+    alert ("you are eligible");
+
+}
+    else {
+        alert ("you are not eligible");
+
+    }
+    
+        
+    }
+    
+    // -------------------------------------------------------------------
+
+
+
+    let run3 =()=>{
+
+
+        let price=prompt("enter price of your item");
+
+
+
+        let dis=confirm(" you want discount ?");
+    
+    
+    if (dis){
+        
+let urprice= (price -(price*0.10)    )
+
+alert(urprice);
+
+    }
+        else {
+            alert ("no dis");
+    
+        }
+        
+        }
+        
+
+        // -------------------------------------------------------------------
+
+
+
+
+        let run4 =()=>{
+
+
+            let marks=prompt("enter your exam marks");
+    
+    
+    
+        
+        
+        if (marks>=33){
+    
+    
+    alert("pass");
+    
+        }
+            else {
+                alert ("FAIL");
+        
+            }
+            
+            }
+
+
+
+
+
+
+   // -------------------------------------------------------------------
+
+
+
+      // -------------------------------------------------------------------
